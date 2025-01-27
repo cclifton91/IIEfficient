2605e5bf7386bbf61ae95a37b69a3dcd7491a54a
Camille <devcclifton@gmail.com>
2025-01-27 11:51:07 -0600

    update from cms

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_WcFO0 from "./images/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md";
-export default new Map([["./images/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md", __ASTRO_IMAGE_IMPORT_WcFO0]]);
+import __ASTRO_IMAGE_IMPORT_aKb61 from "/assets/camicool_A_women_standing_in_front_of_her_brand_new_cafe_at_t_9076ef4f-65d8-438d-839c-c9b2acbea7ca_2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2F2025-01-27-post-via-cms.md";
+export default new Map([["./images/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md", __ASTRO_IMAGE_IMPORT_WcFO0], ["/assets/camicool_A_women_standing_in_front_of_her_brand_new_cafe_at_t_9076ef4f-65d8-438d-839c-c9b2acbea7ca_2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2F2025-01-27-post-via-cms.md", __ASTRO_IMAGE_IMPORT_aKb61]]);
 		
