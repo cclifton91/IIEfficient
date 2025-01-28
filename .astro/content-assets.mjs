b59c2ee0c3cf8774d4a4cb8f3e02dd9dabae577f
Camille <devcclifton@gmail.com>
2025-01-28 13:30:13 -0600

    add Content Feed Section

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_Z1ll8DS from "./assets/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md";
-export default new Map([["./assets/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md", __ASTRO_IMAGE_IMPORT_Z1ll8DS]]);
+import __ASTRO_IMAGE_IMPORT_Z1P4Ury from "./assets/cafe.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject2.md";
+export default new Map([["./assets/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md", __ASTRO_IMAGE_IMPORT_Z1ll8DS], ["./assets/cafe.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject2.md", __ASTRO_IMAGE_IMPORT_Z1P4Ury]]);
 		
