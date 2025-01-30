ae9cd9c8348c7fdb4f31a145096ef568ff179b82
Camille <devcclifton@gmail.com>
2025-01-29 18:19:01 -0600

    update to files

--- src/data/settings.js
+++ src/data/settings.js
@@ -3,11 +3,7 @@ export default {
   description: `Specialized Digital Content Creation for Social Media`,
   url: `https://astro-minimal-starter.netlify.app`, // No trailing slash!
   // JSON LD
-  name: `Example`,
-  // Facebook URL
-  facebookURL: `https://facebook.com`,
-  // Twitter URL
-  twitterURL: `https://twitter.com/jaydanurwin`,
+  name: `II Efficient Media`,
   // Instagram URL
   instagramURL: `https://www.instagram.com/kenny2efficient/`,
   // TikTok URL
