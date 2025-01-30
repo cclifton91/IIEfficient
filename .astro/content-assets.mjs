6912e4cb34ab6925088e8abdaaf7253d25bc3035
Camille <devcclifton@gmail.com>
2025-01-29 18:14:16 -0600

    changed the headings size

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
+import __ASTRO_IMAGE_IMPORT_Z1ll8DS from "./assets/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md";
 import __ASTRO_IMAGE_IMPORT_Z1P4Ury from "./assets/cafe.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject2.md";
 import __ASTRO_IMAGE_IMPORT_Z2n2tpd from "./assets/postphoto3.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject3.md";
 import __ASTRO_IMAGE_IMPORT_Z13WhTu from "./assets/postphoto4.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject4.md";
-import __ASTRO_IMAGE_IMPORT_Z1ll8DS from "./assets/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md";
-export default new Map([["./assets/cafe.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject2.md", __ASTRO_IMAGE_IMPORT_Z1P4Ury], ["./assets/postphoto3.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject3.md", __ASTRO_IMAGE_IMPORT_Z2n2tpd], ["./assets/postphoto4.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject4.md", __ASTRO_IMAGE_IMPORT_Z13WhTu], ["./assets/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md", __ASTRO_IMAGE_IMPORT_Z1ll8DS]]);
+export default new Map([["./assets/indoorgolf.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject1.md", __ASTRO_IMAGE_IMPORT_Z1ll8DS], ["./assets/cafe.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject2.md", __ASTRO_IMAGE_IMPORT_Z1P4Ury], ["./assets/postphoto3.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject3.md", __ASTRO_IMAGE_IMPORT_Z2n2tpd], ["./assets/postphoto4.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject4.md", __ASTRO_IMAGE_IMPORT_Z13WhTu]]);
 		
